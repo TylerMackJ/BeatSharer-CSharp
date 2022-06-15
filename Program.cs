696059b864ccff7e7c0467e7ae305b604db041ae
Tyler Johnson <tylermackj@gmail.com>
2022-06-15 01:46:11 -0500

    Added rerun and colors. Configured for single exe.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Http.Json;
 using System.IO.Compression;
 using dotenv.net;
+using Microsoft.Extensions.Configuration;
 
 public class BeatSharer
 {
@@ -11,59 +12,80 @@ public class BeatSharer
 
     static List<Task> taskList = new List<Task>();
 
-    static string CustomLevelsPath = Path.Combine(Path.GetFullPath("."), "Downloads");
+    static string CustomLevelsPath = Path.GetFullPath(".");
 
     public static async Task Main()
     {
-        // Load secret
-        DotEnv.Load(options: new DotEnvOptions(ignoreExceptions: false));
+        // Load Secrets
+        IConfigurationRoot config = new ConfigurationBuilder().AddUserSecrets<BeatSharer>().Build();
 
         // Get songIDList
         string listIndex = "2";
-        string address = String.Format("https://beat-sharer-default-rtdb.firebaseio.com/{0}.json?auth={1}", listIndex, DotEnv.Read()["SECRET"]);
+        string address = String.Format("https://beat-sharer-default-rtdb.firebaseio.com/{0}.json?auth={1}", listIndex, config["BeatSharer:APIKey"]);
         string songIDs = await client.GetStringAsync(address);
         songIDList = songIDs.Trim('"').Split(',').ToList();
 
-        // Purge existing songs
-        List<string> foundIDs = new List<string>();
-        string[] foundFolders = Directory.GetDirectories(CustomLevelsPath, "*", SearchOption.TopDirectoryOnly);
-        foreach (string folder in foundFolders)
+        while (true)
         {
-            string foundSong = Path.GetFileName(folder);
-            string[] splitTitle = foundSong.Split(" ");
-            if (splitTitle.Length > 0)
+            // Purge existing songs
+            List<string> foundIDs = new List<string>();
+            string[] foundFolders = Directory.GetDirectories(CustomLevelsPath, "*", SearchOption.TopDirectoryOnly);
+            foreach (string folder in foundFolders)
             {
-                foundIDs.Add(splitTitle[0]);
+                string foundSong = Path.GetFileName(folder);
+                string[] splitTitle = foundSong.Split(" ");
+                if (splitTitle.Length > 0)
+                {
+                    foundIDs.Add(splitTitle[0]);
+                }
             }
-        }
-        if (foundIDs.Count == 0)
-        {
-            Console.WriteLine("No songs found. Be sure you have selected the rigth folder");
-            return;
-        }
-        songIDList = songIDList.Except(foundIDs).ToList();
-
-        // Get each song info
-        foreach (string songID in songIDList)
-        {
-            if (songID != "")
+            if (foundIDs.Count == 0)
             {
-                taskList.Add(GetSongInfo(songID));
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("No songs found.");
+                Console.WriteLine("Are you sure {0} is the correct folder?", CustomLevelsPath);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write("(y/n): ");
+                string? userInput = Console.ReadLine();
+                if (userInput == null || userInput != "y")
+                {
+                    return;
+                }
             }
-        }
-        foreach (Task task in taskList)
-        {
-            task.Wait();
-        }
+            songIDList = songIDList.Except(foundIDs).ToList();
 
-        // Download each song
-        foreach (SongInfo songInfo in songInfoList)
-        {
-            taskList.Add(DownloadSong(songInfo));
-        }
-        foreach (Task task in taskList)
-        {
-            task.Wait();
+            // Get each song info
+            taskList.Clear();
+            songInfoList.Clear();
+            foreach (string songID in songIDList)
+            {
+                if (songID != "")
+                {
+                    taskList.Add(GetSongInfo(songID));
+                }
+            }
+            if (taskList.Count == 0)
+            {
+                break;
+            }
+            foreach (Task task in taskList)
+            {
+                await task;
+            }
+            if (songInfoList.Count == 0)
+            {
+                break;
+            }
+
+            // Download each song
+            foreach (SongInfo songInfo in songInfoList)
+            {
+                taskList.Add(DownloadSong(songInfo));
+            }
+            foreach (Task task in taskList)
+            {
+                await task;
+            }
         }
     }
 
@@ -80,12 +102,22 @@ public class BeatSharer
 
     static async Task GetSongInfoTask(string songID)
     {
-        Console.WriteLine("Getting song info for {0}", songID);
-        string address = String.Format("https://api.beatsaver.com/maps/id/{0}", songID);
-        SongInfo? songInfo = await client.GetFromJsonAsync<SongInfo>(address);
-        if (songInfo != null)
+        try
         {
-            songInfoList.Add(songInfo);
+            Console.WriteLine("Getting song info for {0}", songID);
+            string address = String.Format("https://api.beatsaver.com/maps/id/{0}", songID);
+            SongInfo? songInfo = await client.GetFromJsonAsync<SongInfo>(address);
+            if (songInfo != null)
+            {
+                songInfoList.Add(songInfo);
+                Console.WriteLine("Found song info for {0}", songID);
+            }
+        }
+        catch (HttpRequestException)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Could not find song info for {0}", songID);
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
 
@@ -102,14 +134,26 @@ public class BeatSharer
 
     static async Task DownloadSongTask(SongInfo songInfo)
     {
-        if (songInfo.versions != null && songInfo.versions.Length > 0 && songInfo.versions[0].downloadURL != null)
+        try
         {
-            Console.WriteLine("Downloading {0}", songInfo.GetSongTitle());
-            byte[] bytes = await client.GetByteArrayAsync(songInfo.versions[0].downloadURL);
-            MemoryStream compressedStream = new MemoryStream(bytes);
-            ZipArchive zipStream = new ZipArchive(compressedStream);
-            MemoryStream decompressedStream = new MemoryStream();
-            zipStream.ExtractToDirectory(Path.Combine(CustomLevelsPath, songInfo.GetSongTitle()));
+            if (songInfo.versions != null && songInfo.versions.Length > 0 && songInfo.versions[0].downloadURL != null)
+            {
+                Console.WriteLine("Downloading {0}", songInfo.GetSongTitle());
+                byte[] bytes = await client.GetByteArrayAsync(songInfo.versions[0].downloadURL);
+                MemoryStream compressedStream = new MemoryStream(bytes);
+                ZipArchive zipStream = new ZipArchive(compressedStream);
+                MemoryStream decompressedStream = new MemoryStream();
+                zipStream.ExtractToDirectory(Path.Combine(CustomLevelsPath, songInfo.GetSongTitle()));
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Downloaded {0}", songInfo.GetSongTitle());
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+        }
+        catch (HttpRequestException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Failed download for {0}", songInfo.GetSongTitle());
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
 }
